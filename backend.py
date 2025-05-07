39715fdab7177786268b72e2125bdcb097d45eee
Your Name <you@example.com>
2025-05-07 14:52:55 -0400

    Life purchase is officially working

--- backend.py
+++ backend.py
@@ -18,9 +18,9 @@ CORS(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+app.config['MAIL_USERNAME'] = os.getenv('pycodequiz@gmail.com')
+app.config['MAIL_PASSWORD'] = os.getenv('kdolnxgfzrutiomo')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('pycodequiz@gmail.com')
 
 mail = Mail(app)
 
